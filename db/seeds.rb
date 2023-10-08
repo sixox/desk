d24b8bfc490e4753e991181b303ca4c153e4b7e0
sixox <moh3n.dehghani@gmail.com>
2023-10-08 09:57:59 +0330

    vacation finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,24 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 ActiveRecord::Base.transaction do
-	roles = User.roles.keys
+	user_count = User.count
+	VACATION_TYPE = ['Paid', 'Unpaid', 'Medical']
 
-# Iterate through all users and assign a random role
-User.all.each do |user|
-	random_role = roles.sample
-	user.update(role: random_role)
-end
-end
+
+	700.times do
+		  vacation_type = VACATION_TYPE.sample # Randomly select a vacation_type from the array
+
+		  user_id = rand(1..user_count) 
+		  start_date = Faker::Time.between(from: 1.year.ago, to: Time.now)
+		  end_date = start_date + rand(1..10).days
+		  Vacation.create(
+		  	user_id: user_id,
+		  	start_at: start_date,
+		  	end_at: end_date,
+		  	confirm: [true, false].sample,
+		  	vacation_type: vacation_type,
+		  	details: "some reason"
+		  	)
+
+		end
+	end
