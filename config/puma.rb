7a7b3ccda526cde7248a82db03a717d7e24c589e
sixox <moh3n.dehghani@gmail.com>
2023-10-24 12:52:25 +0330

    start deploy8

--- config/puma.rb
+++ config/puma.rb
@@ -4,9 +4,12 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
+
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
+bind "unix://~/desk/tmp/sockets/puma.sock"
+
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
