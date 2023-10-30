f65632bc2394a372bb23d47c143e8021aad26003
sixox <moh3n.dehghani@gmail.com>
2023-10-30 14:32:51 +0330

    update 1

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
+  before_save :set_status
+
   belongs_to :project
   has_many :payment_orders
 
@@ -10,6 +12,25 @@ class Booking < ApplicationRecord
     return 0 if quantity.nil? || filled.nil?
 
     quantity - filled
+  end
+
+  def set_status
+    if vessel_etd.present?
+      self.status = "vessel ETD"
+    elsif custom_submission_date.present?
+      self.status = "custom submission"
+    elsif custom_clearance.present?
+      self.status = "custom clearance"
+    elsif stuffing.present?
+      self.status = "stuffing"
+    elsif pick_up_date.present?
+      self.status = "picked up"
+    elsif factory_order_date.present?
+      self.status = "factory order"
+    else
+      self.status = "not started"
+
+    end
   end
 
 end
