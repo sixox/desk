0d955e6dad727b92a2b9ae27cd56adb8ef461634
sixox <moh3n.dehghani@gmail.com>
2023-11-02 12:27:06 +0330

    update 9

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
 
   def index
     @q = User.ransack(params[:q])
-    @users = @q.result.limit(16).order(:created_at)
+    @users = @q.result.order(:created_at)
   end
 end
