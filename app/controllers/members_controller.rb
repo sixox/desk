1280c16d4a8289fd3b68d70b3aa5c87c31d4c33a
sixox <moh3n.dehghani@gmail.com>
2023-11-18 17:31:32 +0330

    po confirmation tim5

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,5 @@
 class MembersController < ApplicationController
-	before_action :authenticate_user!, only: %i[edit_description update_description edit_personal_details update_personal_details]
+	before_action :authenticate_user!, only: %i[show edit_description update_description edit_personal_details update_personal_details]
 
 	def show
 		@user = User.find(params[:id])
