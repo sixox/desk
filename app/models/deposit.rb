2b481621028b1f83f6c749e4938eb05b80952df9
sixox <moh3n.dehghani@gmail.com>
2024-01-24 17:17:32 +0330

    deposit and menu and transfer3

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -2,5 +2,12 @@ class Deposit < ApplicationRecord
   belongs_to :bank
 
   has_one_attached :document
+  validate :document_presence
+
+  private
+
+  def document_presence
+    errors.add(:document, "must be attached") unless document.attached?
+  end
 
 end
