72e20a12ace57be3f17d423bf8d785d5b70eb2be
sixox <moh3n.dehghani@gmail.com>
2024-07-07 16:08:43 +0330

    big update index and project -edit154

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,18 +14,18 @@ module ApplicationHelper
 		if project.risk.present?
 			case project.risk
 			when 1..15
-				content_tag(:span, "Low Risk", class: 'risk-low')
+				content_tag(:small, "Low Risk", class: 'risk-low')
 			when 16..25
-				content_tag(:span, "Medium Risk", class: 'risk-medium')
+				content_tag(:small, "Medium Risk", class: 'risk-medium')
 			when 26..36
-				content_tag(:span, "High Risk", class: 'risk-high')
+				content_tag(:small, "High Risk", class: 'risk-high')
 			when 37..Float::INFINITY
-				content_tag(:span, "Extreme Risk", class: 'risk-extreme')
+				content_tag(:small, "Extreme Risk", class: 'risk-extreme')
 			else
-				content_tag(:span, "No Risk Data", class: 'no-risk')
+				content_tag(:small, "No Risk Data", class: 'no-risk')
 			end
 		else
-			content_tag(:span, "No Risk Data", class: 'text-secondary')
+			content_tag(:small, "No Risk Data", class: 'text-secondary')
 		end
 
 
