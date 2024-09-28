22df8f99303d22bde81060975b922bed3952e48c
sixox <moh3n.dehghani@gmail.com>
2024-09-28 14:03:40 +0330

    assessment job 22

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -25,11 +25,8 @@ class AssessmentsController < ApplicationController
 
   def form
     @user = User.find(params[:user_id])
-    @assessment_forms = AssessmentForm.joins(:assessment).where(user_id: @user.id, filler_id: @filler.id, assessments: { category: "category 1" })
-    @assessment_forms = AssessmentForm.joins(:assessment).where(user_id: @user.id, filler_id: @filler.id, assessments: { category: "category 1" })
-    @assessment_forms = AssessmentForm.joins(:assessment).where(user_id: @user.id, filler_id: @filler.id, assessments: { category: "category 1" })
-
-
+    @assessment_forms = AssessmentForm.by_user_and_filler(@user, @filler)
+    
   end
 
   def update_form
