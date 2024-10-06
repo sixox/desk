3d007bc76af6f4a32aa711456d2432f755724137
sixox <moh3n.dehghani@gmail.com>
2024-10-06 11:08:32 +0330

    dashboard bank08

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -27,11 +27,37 @@ class HomeController < ApplicationController
 
     #accounting tables start
     @latest_payment_orders = PaymentOrder
-                          .order(created_at: :desc)
-                          .limit(14)
+    .order(created_at: :desc)
+    .limit(14)
     @transactions = Transaction.all.includes(:transactionable).order(created_at: :desc).limit(14)
     @banks = Bank.all
     # accounting tables end
+
+    # sales started
+    @total_pi_by_month = (0..5).map do |n|
+      # Move `n` months back from the current date
+      month_start = Date.current.advance(months: -n).beginning_of_month
+      month_end = Date.current.advance(months: -n).end_of_month
+
+      total = Pi.where(created_at: month_start..month_end).reduce(0) do |sum, pi|
+        if pi.currency == "dirham"
+          sum += pi.total_price.to_i
+        elsif pi.currency == "dollar"
+          sum += pi.total_price.to_i * 3.67
+        else
+          sum
+        end
+      end
+
+      { month: month_start.strftime("%B"), total: total }
+    end
+
+    # Extract the data for the chart
+    @months = @total_pi_by_month.map { |data| data[:month] }
+    @totals = @total_pi_by_month.map { |data| data[:total] }
+
+
+    # sales end
 
 
 
