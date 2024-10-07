8a338a35c710188cf339dea75b58449dc3994f6e
sixox <moh3n.dehghani@gmail.com>
2024-10-07 15:57:11 +0330

    dashboard pos0119

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     @q = User.ransack(params[:q])
     @users = @q.result.order(:created_at).page(params[:page]).per(10)
     if current_user.id == 30 || current_user.admin?
-      @allocations = BallanceProjects.all
+      @allocations = BallanceProject.all
     end
   end
 
