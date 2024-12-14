79ba7ee9fb51d4f1a104d41ee1c9e146cff41f3b
sixox <moh3n.dehghani@gmail.com>
2024-12-14 12:51:21 +0330

    rialreceipt0034

--- app/models/rial_receipt.rb
+++ app/models/rial_receipt.rb
@@ -2,5 +2,5 @@ class RialReceipt < ApplicationRecord
   belongs_to :payment_order
 
   validates :payment_order, presence: true
-  validates :in_words, :details, :receiver, :from_source, :payment_date,  presence: true
+  validates :in_words, :details, :receiver, :payment_date,  presence: true
 end
