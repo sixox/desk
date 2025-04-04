d4ed8b831d69c4df70cb85622919e6c0c4166e1c
sixox <moh3n.dehghani@gmail.com>
2025-04-04 12:00:43 +0330

    project name regex new01

--- app/models/project.rb
+++ app/models/project.rb
@@ -15,7 +15,7 @@ class Project < ApplicationRecord
   has_secure_password
 
 
-  validates :number, presence: true, format: { with: /\A14\d{2}-\d{4}\z/, message: "must be in the format '14xx-xxxx'" }, uniqueness: true
+  validates :number, presence: true, format: { with: /\A14\d{2}-(?:\d{4}|10\d{3})\z/, message: "must be in the format 14xx-xxxx or 14xx-10xxx" }, uniqueness: true
   validates :password, presence: true, on: :create
 
 
