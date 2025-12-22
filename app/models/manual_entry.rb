8a96d4c7f1e8b41677fc7330ae58cb2bbcd97a34
sixox <moh3n.dehghani@gmail.com>
2025-12-22 12:24:10 +0330

    payrol09

--- app/models/manual_entry.rb
+++ app/models/manual_entry.rb
@@ -1,5 +1,7 @@
 # app/models/manual_entry.rb
 class ManualEntry < ApplicationRecord
+    before_validation :sync_occurred_on_from_occurred_at
+
   belongs_to :user
 
   validates :occurred_at, presence: true
@@ -11,6 +13,14 @@ class ManualEntry < ApplicationRecord
   scope :on_date, ->(date) { where(occurred_at: date.beginning_of_day..date.end_of_day) }
 
   private
+
+  
+  def sync_occurred_on_from_occurred_at
+    return if occurred_on.present?
+
+    # اگر occurred_at داریم، occurred_on = تاریخش
+    self.occurred_on = occurred_at.to_date if occurred_at.present?
+  end
 
   def unique_type_per_day
     return if occurred_at.blank?
