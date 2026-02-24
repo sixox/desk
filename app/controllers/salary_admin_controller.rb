2b26b3599729dc15b699b740fdb5106467a7268f
sixox <moh3n.dehghani@gmail.com>
2026-02-24 15:03:43 +0330

    payroll new 03 profile24

--- app/controllers/salary_admin_controller.rb
+++ app/controllers/salary_admin_controller.rb
@@ -1,6 +1,20 @@
 # app/controllers/salary_admin_controller.rb
 class SalaryAdminController < ApplicationController
-  def index
-    @latest_month = ShamsiMonth.order(start_at: :desc).first
+  before_action :authenticate_user!
+
+# app/controllers/salary_admin_controller.rb
+def index
+  @months = ShamsiMonth.order(id: :desc)
+
+  # قبلا احتمالا این بود:
+  # @latest_month = @months.first
+  #
+  # ✅ حالا: اگر month_id تو params بود، همونو فعال کن؛ وگرنه آخرین ماه
+  @latest_month =
+  if params[:month_id].present?
+    ShamsiMonth.find_by(id: params[:month_id]) || @months.first
+  else
+    @months.first
   end
+end
 end
